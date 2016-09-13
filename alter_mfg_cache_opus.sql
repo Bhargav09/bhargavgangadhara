8639a8fac3968748532bc8309908235bc2a0f81b
BHARGAV GANGADHARA <bhargav.gangadhara@aol.com>
2016-09-13 02:20:04 -0400

    Update alter_mfg_cache_opus.sql

--- alter_mfg_cache_opus.sql
+++ alter_mfg_cache_opus.sql
@@ -1,11 +1,3 @@
-/************************************************************************************
- File: alter_egenie_changes.sql
- Description: These tables are altered to accommodate the changes that eGenie requires
- Auther:Selvaraj Aran(aselvara)
- Date  :08/24/2010
- Copyright (c) 2010 by Cisco Systems, Inc.
- All rights reserved.A
-*************************************************************/
 alter table mfg_cache_opus
 add (PARENT_PRODUCT_NAME     VARCHAR2(18 BYTE) );
 
